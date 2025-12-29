1cd2211bf21e09a40127fa90500c0d7e959d7c61
Wilfred Hughes <me@wilfred.me.uk>
2025-12-29 03:23:48 -0800

    Fix lowering with supertrait predicates
    Previously both valid and invalid Rust code could crash r-a due to a
    cyclic query during lowering.

--- crates/hir-ty/src/tests/regression.rs
+++ crates/hir-ty/src/tests/regression.rs
@@ -2563,3 +2563,51 @@ fn main() {
         "#,
     );
 }
+
+#[test]
+fn issue_21006_generic_predicates_for_param_supertrait_cycle() {
+    check_no_mismatches(
+        r#"
+trait VCipherSuite {}
+
+trait CipherSuite
+where
+    OprfHash<Self>: Hash,
+{
+}
+
+type Bar<CS: CipherSuite> = <CS::Baz as VCipherSuite>::Hash;
+
+type OprfHash<CS: CipherSuite> = <CS::Baz as VCipherSuite>::Hash;
+
+impl<CS: CipherSuite> Foo<CS> {
+    fn seal() {}
+}
+        "#,
+    );
+}
+
+#[test]
+fn issue_21006_self_assoc_trait() {
+    check_types(
+        r#"
+trait Baz {
+    fn baz(&self);
+}
+
+trait Foo {
+    type Assoc;
+}
+
+trait Bar: Foo
+where
+    Self::Assoc: Baz,
+{
+    fn bar(v: Self::Assoc) {
+        let _ = v.baz();
+        //  ^ ()
+    }
+}
+        "#,
+    );
+}
